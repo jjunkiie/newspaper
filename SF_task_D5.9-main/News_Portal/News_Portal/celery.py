2b8e448cc2ac5287af0ca141583ce5fcfa153c7b
jjunkiie <137442314+jjunkiie@users.noreply.github.com>
2024-03-27 23:14:23 +0300

    das

--- SF_task_D5.9-main/News_Portal/News_Portal/celery.py
+++ SF_task_D5.9-main/News_Portal/News_Portal/celery.py
@@ -1,9 +1,29 @@
 import os
-from celery import Celery
- 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcdonalds.settings')
- 
-app = Celery('mcdonalds')
-app.config_from_object('django.conf:settings', namespace = 'CELERY')
 
-app.autodiscover_tasks()
+from .settings import INSTALLED_APPS
+from celery import Celery
+from celery.schedules import crontab
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
+
+app = Celery('NewsPaper')
+
+
+app.config_from_object('django.conf:settings') 
+
+
+app.autodiscover_tasks(INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
+
+
+app.conf.beat_schedule = {
+    'send_mailing_monday_8am': {
+        'task': 'news_paper.tasks.do_mailing',
+        'schedule': crontab(day_of_week=1, hour=8, minute=0),
+    },
+}
